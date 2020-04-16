1c525c376adc03c6b06693b891404ed00b4e0f41
museongseo <to1376@naver.com>
2020-04-16 14:57:02 +0900

    merge codes into lab1 file

--- lab1_sched.c
+++ lab1_sched.c
@@ -27,11 +27,61 @@
 #include <assert.h>
 #include <pthread.h>
 #include <asm/unistd.h>
+#include <stdio.h>
+#include "ListBaseQueue.h"
+
 
 #include "lab1_sched_types.h"
 
-/*i
- * you need to implement FIFO, RR, MLFQ scheduler and bonus to do stride scheduler
- */
+/******************************** Queue Implementation  *************************************/
+
+void QueueInit(Queue * pq)
+{
+   pq->front = NULL;
+   pq->rear = NULL;
+}
+
+int QIsEmpty(Queue * pq)
+{
+   if(pq->front == NULL)
+      return TRUE;
+   else
+      return FALSE;
+}
+
+void Enqueue(Queue * pq, Data data)
+{
+   Node * newNode = (Node*)malloc(sizeof(Node));
+   newNode->next = NULL;
+   newNode->data = data;
+
+   if(QIsEmpty(pq))
+   {
+      pq->front = newNode;
+      pq->rear = newNode;
+   }
+   else
+   {
+      pq->rear->next = newNode;
+      pq->rear = newNode;
+   }
+}
+
+Data Dequeue(Queue * pq)
+{
+   Node * delNode;
+   Data retData;
+
+   if(QIsEmpty(pq))
+   {
+      printf("Queue Memory Error!");
+      exit(-1);
+   }
+
+   delNode = pq->front;
+   retData = delNode->data;
+   pq->front = pq->front->next;
+
+/************************************************ FIFO Implementation  **********************************************/
 
 
